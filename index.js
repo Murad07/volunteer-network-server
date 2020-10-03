d2c4a2edfa0a246f7077cd80be235ef70d1610e5
Murad Hossain <sliverboymurad@gmail.com>
2020-10-03 15:08:02 +0600

    insert fake data and read

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const nodemailer = require('nodemailer');
+
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kzt0x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Thank you for calling me.');
-});
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(5000);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const volunteersCollection = client
+    .db('volunteerNetwork')
+    .collection('volunteers');
+
+  //Add all data from fakedata api
+  app.post('/addVolunteer', (req, res) => {
+    const volunteers = req.body;
+
+    volunteersCollection.insertMany(volunteers).then((result) => {
+      res.send(result.insertedCount);
+    });
+  });
+
+  //Read data from server - all volunteers
+  app.get('/volunteers', (req, res) => {
+    volunteersCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+});
